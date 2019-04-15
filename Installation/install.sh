782ab312c35c6083a3319969cd0d23a1ecdc8f60
blackcape29 <nikhilbish@gmail.com>
2019-04-15 07:00:12 +0530

    here we go

---
+++ Installation/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pip install selenium
+#adding geckodriver to your system
+wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+tar xvfz geckodriver-v0.24.0-linux64.tar.gz
+mv geckodriver ~/.local/bin
+rm -rf geckodriver-v0.24.0-linux64.tar.gz
+#Thats it, jai mata di
--- webscraping.py
+++ webscraping.py
@@ -1,14 +1,53 @@
-#import the library used to query a website
-import urllib.request
-#specify the url
-nlp = "http://nlp.stanford.edu:8080/corenlp/"
-#Query the website and return the html to the variable 'page'
-page = urllib.request.urlopen(nlp)
-#import the Beautiful soup functions to parse the data returned from the website
-from bs4 import BeautifulSoup
-#Parse the html in the 'page' variable, and store it in Beautiful Soup format this will be done in lxml parsing
-#print(soup)
 from selenium import webdriver
-
-browser = webdriver.Firefox()
-#browser.get('http://seleniumhq.org/')
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+#Setting up URL
+nlp = "http://nlp.stanford.edu:8080/corenlp/"
+print("Sit Back and Relax\n")
+#taking input
+with open('Data/data.txt', 'r') as myfile:
+	corpus = myfile.read().replace('\n', '')
+#Setting up Driver
+driver = webdriver.Firefox()
+print("Opened browser....\n")
+#initializing URL
+driver.get(nlp)
+#Selecting viewing option
+view = Select(driver.find_element_by_name("outputFormat"))
+view.select_by_visible_text('CoNLL')
+#Entering text in Text Area
+text_area = driver.find_element_by_name("input")
+text_area.send_keys(corpus)
+print("Entered Text....\n")
+#Clicking on submit
+driver.find_element_by_name("Process").click()
+#Getting element with output, stored under pre tag
+text = driver.find_elements_by_tag_name("pre")
+print("Got Data....\n")
+#taking element from list
+for x in text:
+	data = x.text
+#spliting the words int the list
+data=data.replace('\n',' ',)
+list1=data.split(' ')
+#close
+driver.close()
+print("Closed Browser....\n")
+k=0
+main=[]
+list3=[]
+for x in list1:
+	if (k%7)!=0:
+		list3.append(x)
+	else:
+		if k!=0:
+			main.append(list3)
+		list3=[]
+	k+=1
+#main is the 2d array of processed data with
+# main[][0] words
+# main[][1] be their root
+# main[][2] be their pos tags
+# main[][3] be their Named Entities
+# main[][4] , main[][5] idk sorry 	
+print(main)
