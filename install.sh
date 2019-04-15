572c3e706f4b33479eb6a9e986ee76f5e31bc065
Nikhil Bishnoi <nikhilbish@gmail.com>
2019-04-15 07:01:26 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-pip install selenium
-pip install beautifulsoup4
-#adding geckodriver to your system
-wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
-tar xvfz geckodriver-v0.18.0-linux64.tar.gz 
-mv geckodriver ~/.local/bin
